7d8f2616c50fc88007843008bbdc9dbbc059aa4f
Kevin Yu <kevinyu567@gmail.com>
2024-09-24 17:57:11 -0400

    Parse challenge metadata

---
+++ src/challs.ts
@@ -0,0 +1,58 @@
+import { readFile } from 'node:fs/promises';
+
+
+export type ChallengeData = {
+    author: string,
+    category: string,
+    description: string,
+    flag: string,
+    name: string,
+    points: {
+        min: number,
+        max: number
+    },
+    tiebreakEligible: boolean
+}
+
+/**
+ * Gets the rCTF challenge data for a given category and challenge name.
+ *
+ * @param base The base challenge directory.
+ * @param category The category directory.
+ * @param name The challenge directory.
+ *
+ * @returns The parsed data, or `null` if the challenge should be skipped.
+ */
+export async function getChallengeMetadata(base: string, category: string, name: string) {
+    let raw;
+    try {
+        raw = (await readFile(`${base}/${category}/${name}/chal.json`)).toString();
+    } catch {
+        throw new Error(`Challenge data not found for \`${category}/${name}\`.`)
+    }
+
+    const data = JSON.parse(raw);
+    if (!('author' in data) || typeof data.author !== 'string')
+        throw new Error(`Field \`author\` for \`${category}/${name}\` missing or wrong type.`);
+    if (!('description' in data) || typeof data.description !== 'string')
+        throw new Error(`Field \`author\` for \`${category}/${name}\` missing or wrong type.`);
+    if (!('flag' in data) || typeof data.description !== 'string')
+        throw new Error(`Field \`flag\` for \`${category}/${name}\` missing or wrong type.`);
+    if (!('name' in data) || typeof data.name !== 'string')
+        throw new Error(`Field \`name\` for \`${category}/${name}\` missing or wrong type.`);
+
+    const ret: ChallengeData = {
+        author: data.author,
+        category,
+        description: data.description,
+        flag: data.flag,
+        name: data.name,
+        points: {
+            min: 100,
+            max: 500
+        },
+        tiebreakEligible: true // TODO
+    }
+
+    return ret;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import { readdir } from 'node:fs/promises';
+import { getChallengeMetadata } from './challs';
 
 
 async function run() {
@@ -10,20 +11,25 @@ async function run() {
         const apiBase = new URL('/api/v1', url).href;
         core.info(`API_BASE: ${apiBase}`);
 
+        const baseDir = './src' // TODO
+
         // Parse categories from subdirectories of challenge directory.
-        const categories = (await readdir('./src', { withFileTypes: true }))
+        const categories = (await readdir(baseDir, { withFileTypes: true }))
             .filter((d) => d.isDirectory())
             .map((d) => d.name);
 
         core.info(`Categories: [${categories.join(', ')}]`);
 
         for (const category of categories) {
-            const challs = (await readdir(`./src/${category}`, { withFileTypes: true }))
+            const challs = (await readdir(`${baseDir}/${category}`, { withFileTypes: true }))
                 .filter((d) => d.isDirectory())
                 .map((d) => d.name);
 
             for (const chall of challs) {
                 core.info(`Found chall \`${category}/${chall}\``);
+
+                const data = await getChallengeMetadata(baseDir, category, chall);
+                core.info(JSON.stringify(data));
             }
         }
 
